573b49dec29197994e987607cba63c385be30c6c
kamesh <kameshvishwakarma84@gmail.com>
2023-05-04 19:12:18 +0530

    Read & display integer

---
+++ program2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n ;
+    clrscr();
+    printf("entre any integer: ");
+    scanf("%d",&n);
+
+    // For displaying 
+    printf("Your integer is =%d",n);
+    return 0; 
+}
