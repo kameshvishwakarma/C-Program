c27f5de5dd702909579dd079223ced7c121a5dcd
kamesh <kameshvishwakarma84@gmail.com>
2023-05-08 19:07:36 +0530

    sum of two number

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 In this repository we are trying to add on daily bases C code.
 
 Que 1. writer a C program to print hello word.
+Que 2. write a c program to add two number.
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b, sum=0;
+    printf("enter any number:");
+    scanf("%d%d",&a,&b);
+    sum=a+b;gi
+    printf("sum=%d",sum);
+    return 0;12
+
+}
