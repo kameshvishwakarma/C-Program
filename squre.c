50b2ccbba5c6d7299ee1a3af005d24eca4419d0a
kamesh <kameshvishwakarma84@gmail.com>
2023-05-12 23:39:11 +0530

    square

---
+++ squre.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n , square;
+    printf("enter any number:");
+    scanf("%d",&n);
+    square=n*n;
+    printf("the square of number = %d",square);
+    return 0;
+    
+}
